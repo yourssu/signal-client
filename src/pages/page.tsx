0e82f675fdf1b21415f71d129a4533d170abdb37
Hyomin Koo <me@eatsteak.dev>
2025-04-17 21:00:01 +0900

    feat: impl count query

--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -1,17 +1,22 @@
 import { Button } from "@/components/ui/button";
-import React from "react";
+import React, { useMemo } from "react";
 import { useNavigate } from "react-router";
 import feelings from "@/assets/feelings.png";
+import { useCountProfile } from "@/queries/profile";
 
 const HomePage: React.FC = () => {
   const navigate = useNavigate();
+
+  const { data } = useCountProfile();
+
+  const count = useMemo(() => data?.count ?? 0, [data?.count]);
 
   return (
     <div className="flex flex-col items-center gap-24 justify-center min-h-screen p-4">
       <div className="flex flex-col justify-center">
         <h1 className="text-2xl break-keep mb-4 text-center">
           당신의 시그널을 <br />
-          <span className="text-primary">153명</span>이 기다리고 있어요
+          <span className="text-primary">{count}명</span>이 기다리고 있어요
         </h1>
         <p>시그널이 이어준 인연, 이번 학기에 어쩌구</p>
       </div>
--- src/queries/profile.ts
+++ src/queries/profile.ts
@@ -4,11 +4,27 @@ import {
   NicknameCreatedResponse,
   NicknameGeneratedRequest,
   ProfileContactResponse,
+  ProfileCountResponse,
   ProfileCreatedRequest,
   ProfileResponse,
   TicketConsumedRequest,
 } from "@/types/profile";
 import { SuccessResponse } from "@/types/common";
+
+export const useCountProfile = () => {
+  return useQuery({
+    queryKey: ["profiles", "count"],
+    queryFn: async () => {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_BASE_URL ?? ""}/api/profiles/count`
+      );
+
+      const data =
+        (await response.json()) as SuccessResponse<ProfileCountResponse>;
+      return data.result;
+    },
+  });
+};
 
 export const useRandomProfile = (uuid: string, gender: Gender) => {
   return useQuery({
