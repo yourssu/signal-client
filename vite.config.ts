2ec9d8b96fa2fa6c2b7855f5d54682359b362da6
EATSTEAK <me@eatsteak.dev>
2025-05-03 22:33:48 +0900

    feat: install `vite-plugin-image-optimizer`

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwind from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwind(), tsconfigPaths()],
+  plugins: [react(), tailwind(), tsconfigPaths(), ViteImageOptimizer()],
 });
