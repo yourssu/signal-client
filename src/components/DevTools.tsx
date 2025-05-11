a84120a6581e9e714b684f68b273704147a22265
EATSTEAK <me@eatsteak.dev>
2025-05-11 16:23:39 +0900

    feat: issue ticket in devtools

--- src/components/DevTools.tsx
+++ src/components/DevTools.tsx
@@ -14,8 +14,11 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
+import { Input } from "@/components/ui/input";
+import { useIssueTicket } from "@/hooks/queries/viewers";
 import { useAtomValue } from "jotai";
 import { Code } from "lucide-react";
+import { useRef } from "react";
 import { createPortal } from "react-dom";
 
 const IS_LOCAL = import.meta.env.NODE_ENV !== "production";
@@ -31,6 +34,28 @@ export const DevTools = () => {
   const userGender = useAtomValue(userGenderAtom);
   const userProfile = useAtomValue(userProfileAtom);
   const viewerSelf = useAtomValue(viewerSelfAtom);
+  const verificationRef = useRef<HTMLInputElement>(null);
+  const countRef = useRef<HTMLInputElement>(null);
+  const { mutateAsync: issueTicket } = useIssueTicket();
+
+  const handleVerification = async () => {
+    if (!verificationRef.current || !countRef.current) return;
+    const verificationCode = Number(verificationRef.current.value);
+    const ticket = Number(countRef.current.value);
+    if (!verificationCode || isNaN(ticket)) return;
+
+    try {
+      await issueTicket({
+        secretKey: "DonaEmilyEatSteakLeopold",
+        verificationCode,
+        ticket,
+      });
+      alert("티켓 발급 완료");
+    } catch (error) {
+      console.error(error);
+      alert("티켓 발급 실패");
+    }
+  };
   return createPortal(
     <Drawer>
       <DrawerTrigger asChild>
@@ -50,23 +75,41 @@ export const DevTools = () => {
             {userUuid && `UUID: ${userUuid}`}
           </DrawerDescription>
         </DrawerHeader>
-        <div className="flex flex-col gap-2 h-64 overflow-y-auto overflow-x-hidden mx-4">
-          <div>
-            <h1 className="font-medium text-lg">PROFILE</h1>
-            <p>userGender: {userGender}</p>
-            <p>userProfile</p>
-            <pre className="whitespace-pre-wrap">
-              {JSON.stringify(userProfile)}
-            </pre>
+        <div className="flex flex-col gap-2 max-h-64 overflow-y-auto overflow-x-hidden mx-4">
+          <div className="flex flex-col gap-1">
+            <h1 className="font-medium text-lg">Verification</h1>
+            <Input
+              ref={verificationRef}
+              placeholder="Enter verification code..."
+            />
+            <Input
+              ref={countRef}
+              type="number"
+              placeholder="Enter ticket count..."
+            />
+            <Button onClick={handleVerification}>Provide Ticket</Button>
           </div>
-          <div>
-            <h1 className="font-medium text-lg">VIEWER</h1>
-            <p>desiredGender: {desiredGender}</p>
-            <p>self</p>
-            <pre className="whitespace-pre-wrap">
-              {JSON.stringify(viewerSelf)}
-            </pre>
-          </div>
+          <details>
+            <summary className="font-medium text-lg cursor-pointer">
+              Debug Information
+            </summary>
+            <div className="flex flex-col gap-1">
+              <h1 className="font-medium text-lg">PROFILE</h1>
+              <p>userGender: {userGender}</p>
+              <p>userProfile</p>
+              <pre className="whitespace-pre-wrap break-all">
+                {JSON.stringify(userProfile)}
+              </pre>
+            </div>
+            <div>
+              <h1 className="font-medium text-lg">VIEWER</h1>
+              <p>desiredGender: {desiredGender}</p>
+              <p>self</p>
+              <pre className="whitespace-pre-wrap break-all">
+                {JSON.stringify(viewerSelf)}
+              </pre>
+            </div>
+          </details>
         </div>
         <DrawerFooter>
           <Button onClick={resetData}>데이터 초기화</Button>
