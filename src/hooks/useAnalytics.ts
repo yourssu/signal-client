c611ddf92bf9d00425b9f3a03730b78972b1e746
EATSTEAK <me@eatsteak.dev>
2025-05-20 21:04:41 +0900

    feat: add viewer information to analytics

--- src/hooks/useAnalytics.ts
+++ src/hooks/useAnalytics.ts
@@ -6,11 +6,13 @@ import { useAtomValue } from "jotai";
 import { useEffect } from "react";
 import ReactGA4 from "react-ga4";
 import Clarity from "@microsoft/clarity";
+import { viewerSelfAtom } from "@/atoms/viewerSelf";
 
 export const useAnalytics = () => {
   const uuid = useUserUuid();
   const gender = useAtomValue(userGenderAtom);
   const profile = useAtomValue(userProfileAtom);
+  const viewerSelf = useAtomValue(viewerSelfAtom);
 
   useEffect(() => {
     if (GA_ID)
@@ -27,11 +29,14 @@ export const useAnalytics = () => {
     if (GA_ID)
       ReactGA4.set({
         user_properties: {
+          profile_registered: !!profile,
           gender: gender ?? undefined,
           mbti: profile?.mbti ?? undefined,
           department: profile?.department ?? undefined,
           birth_year: profile?.birthYear?.toString() ?? undefined,
+          ticket: viewerSelf?.ticket ?? undefined,
+          used_ticket: viewerSelf?.usedTicket ?? undefined,
         },
       });
-  }, [gender, profile]);
+  }, [gender, profile, viewerSelf]);
 };
