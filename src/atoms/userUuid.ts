495a7b4f0972daa538fc9c95bf37a584aee4aebb
Koo Hyomin <me@eatsteak.dev>
2025-09-07 21:46:44 +0900

    feat!: remove `userUuid` atom to replace with queries

--- src/atoms/userUuid.ts
+++
@@ -1,7 +0,0 @@
-import { atomWithStorage } from "jotai/utils";
-export const userUuidAtom = atomWithStorage<string | null>(
-  "user.uuid", // localStorage key
-  null,
-  undefined,
-  { getOnInit: true }, // optional, default is false
-);
