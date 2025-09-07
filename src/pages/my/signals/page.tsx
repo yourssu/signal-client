dd7859ceed7b8967f790eb94c338bd3ce40585e2
EATSTEAK <me@eatsteak.dev>
2025-09-08 04:55:50 +0900

    feat(my/signals): revise empty view

--- src/pages/my/signals/page.tsx
+++ src/pages/my/signals/page.tsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router";
 import { useAtomValue } from "jotai";
-import { Button, buttonVariants } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import TopBar from "@/components/Header";
 import {
   contactedProfilesAtom,
   savedProfilesAtom,
 } from "@/atoms/viewerProfiles";
 import { ProfileResponse } from "@/types/profile";
-import { cn } from "@/lib/utils";
 import { ScrollableCards } from "@/components/my/saved/ScrollableCards";
 import { ENABLE_SAVED } from "@/env";
+import main from "@/assets/home/main.png";
 
 const ContactedProfilesPage: React.FC = () => {
   const navigate = useNavigate();
@@ -25,10 +25,6 @@ const ContactedProfilesPage: React.FC = () => {
 
   const count = `${savedProfiles.length}`.padStart(2, "0");
 
-  const handleBack = () => {
-    navigate(-1); // Go back one step in browser history
-  };
-
   const handleViewContact = () => {
     if (!profile?.profileId) return;
     navigate(`/profile/contact?id=${profile.profileId}&from=saved`, {
@@ -40,7 +36,7 @@ const ContactedProfilesPage: React.FC = () => {
 
   return (
     <div className="w-full h-full flex flex-col items-center">
-      <TopBar onBack={handleBack} />
+      <TopBar onBack="/my" />
       {profile ? (
         <>
           <div className="flex flex-col gap-4 items-center w-full max-w-md grow p-6">
@@ -78,32 +74,28 @@ const ContactedProfilesPage: React.FC = () => {
       ) : (
         <>
           <div className="flex flex-col gap-4 items-center w-full max-w-md grow p-6">
-            <div className="flex flex-col items-start w-full">
-              {ENABLE_SAVED ? (
-                <h1 className="text-2xl font-semibold text-stone-700">
-                  아직 저장된 사람이 없어요
-                  <br />
-                  마음에 드는 사람을 저장해보세요
-                </h1>
-              ) : (
-                <h1 className="text-2xl font-semibold text-stone-700">
-                  아직 시그널을 보낸 사람이 없어요
-                  <br />
-                  <span className="text-primary">
-                    마음에 드는 사람에게 시그널을 보내보세요
-                  </span>
-                </h1>
-              )}
+            <div className="flex flex-col items-center justify-center w-full grow">
+              <h1 className="text-2xl font-semibold text-stone-700">
+                아직 보낸 시그널이 없어요
+              </h1>
+              <p>지금 바로 당신의 첫 시그널을 보내볼까요?</p>
+              <div className="w-full max-w-[280px]">
+                <img src={main} alt="Cat Character" className="w-full h-auto" />
+              </div>
             </div>
-            <Link
-              to="/profile"
-              className={cn(
-                buttonVariants({ size: "xl" }),
-                "w-full rounded-3xl mt-auto",
-              )}
-            >
-              시그널 보내러 가기
-            </Link>
+            <div className="flex gap-4 w-full relative z-50">
+              <Button
+                variant="secondary"
+                size="xl"
+                className="basis-1/3 rounded-3xl text-primary"
+                asChild
+              >
+                <Link to="/">홈으로</Link>
+              </Button>
+              <Button size="xl" className="grow rounded-3xl" asChild>
+                <Link to="/profile">시그널 보내기</Link>
+              </Button>
+            </div>
           </div>
         </>
       )}
