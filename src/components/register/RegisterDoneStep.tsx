61c6398364afab776d0b91576c17524e80db4c19
EATSTEAK <me@eatsteak.dev>
2025-09-08 05:13:41 +0900

    lint: prettier

--- src/components/register/RegisterDoneStep.tsx
+++ src/components/register/RegisterDoneStep.tsx
@@ -45,10 +45,7 @@ const RegisterDoneStep: React.FC<RegisterDoneStepProps> = ({
         이용권 구매하기
       </Button>
 
-      <LoginDrawer
-        open={loginDrawerOpen}
-        onOpenChange={setLoginDrawerOpen}
-      />
+      <LoginDrawer open={loginDrawerOpen} onOpenChange={setLoginDrawerOpen} />
     </div>
   );
 };
