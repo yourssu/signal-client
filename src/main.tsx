8fbe169d17ce2ded4f77277a266bcfe6ce83e769
Koo Hyomin <me@eatsteak.dev>
2025-09-13 15:26:38 +0900

    feat: init `@resvg/resvg-wasm`

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { initMocks } from "./mocks/browser";
+import { initWasm } from "@resvg/resvg-wasm";
 
 if (import.meta.env.DEV) initMocks();
+await initWasm(fetch("/wasm/index_bg.wasm"));
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
